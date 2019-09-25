5cfd51908970e72d805c0d745fd5d3733e92a610
Jonas Horvei <jonashorvei1@gmail.com>
2019-09-25 07:01:59 +0200

    display additional information when div is clicked

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    font: 'italic',
+    borderWidth: 2,
+    marginBottom: 10
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : ''}
+  const showWhenVisible = { display: visible ? '' : 'none'}
+
+  const toggleVisibility = () => {
+      setVisible(!visible)
+  }
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <div onClick={toggleVisibility}>{blog.title} {blog.author}</div>    
+      </div>
+      <div style={showWhenVisible}>
+        <div onClick={toggleVisibility}>{blog.title} {blog.author}</div>
+          <div>{blog.url}</div>
+          <div>{blog.likes} likes</div>
+          {blog.user !== undefined && blog.user.name !== undefined &&
+            <div>{blog.user.name}</div>
+          }
+          {/* {console.log(blog.user.username)} */}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
